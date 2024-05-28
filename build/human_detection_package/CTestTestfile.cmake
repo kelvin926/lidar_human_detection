de728a3103cfeaba9b0dea6c95963dd3168a991a
kelvin926 <kelvin926@korea.ac.kr>
2024-05-28 16:40:25 +0900

    1st fin

---
+++ build/human_detection_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hyunseo/human_detection_package
+# Build directory: /home/hyunseo/human_detection_package/build/human_detection_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ install/human_detection_package/share/human_detection_package/launch/velodyne_launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='velodyne_driver',
+            executable='velodyne_driver_node',
+            name='velodyne_driver',
+            parameters=[{
+                'frame_id': 'velodyne',
+                'model': 'VLP16'
+            }],
+            output='screen'
+        ),
+        Node(
+            package='velodyne_pointcloud',
+            executable='velodyne_transform_node',
+            name='velodyne_transform',
+            parameters=[{
+                'min_range': 0.4,
+                'max_range': 130.0,
+                'calibration': '/home/hyunseo/human_detection_package/config/VLP-16db.yaml',
+                'model': 'VLP16'
+            }],
+            output='screen'
+        ),
+        Node(
+            package='human_detection_package',
+            executable='human_detection_node.py',
+            name='human_detection_node',
+            output='screen'
+        ),
+    ])
+
